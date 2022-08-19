434abd0c1c01ec936ed516cfc5a0156e7d777ca9
anurag14201 <108300944+anurag14201@users.noreply.github.com>
2022-08-20 02:44:36 +0600

    updates

--- shopping-cart-main/cart.js
+++ shopping-cart-main/cart.js
@@ -1,6 +1,37 @@
-document.getElementById('cart-but').addEventListener('click', function () {
-    const plusClicked = document.getElementById('input-value');
-    // const inputValue = parseInt(plusClicked).value + 1;
-    const inputValue = parseInt(plusClicked.value) + 1;
-    console.log(inputValue);
+function getUpDates(product, price, isIncreasing) {
+    const plusClicked = document.getElementById(product + '-Number');
+    let inputValue = plusClicked.value;
+    if (isIncreasing == true) {
+        inputValue = parseInt(inputValue) + 1;
+
+    }
+    else if (inputValue > 0) {
+        inputValue = parseInt(inputValue) - 1;
+
+    }
+    plusClicked.value = inputValue;
+    const caseTotal = document.getElementById(product + '-price');
+    caseTotal.innerText = inputValue * price;
+}
+document.getElementById('phone-plus').addEventListener('click', function () {
+    getUpDates('phone', '1219', true);
 })
+document.getElementById('phone-minus').addEventListener('click', function () {
+    getUpDates('phone', '1219', false);
+})
+
+
+
+document.getElementById('case-plus').addEventListener('click', function () {
+    // const plusClicked = document.getElementById('input-value');
+    // const inputValue = plusClicked.value;
+    // plusClicked.value = parseInt(inputValue) + 1;
+    getUpDates('case', '59', true);
+})
+document.getElementById('case-minus').addEventListener('click', function () {
+    // const plusClicked = document.getElementById('input-value');
+    // const inputValue = plusClicked.value;
+    // plusClicked.value = parseInt(inputValue) - 1;
+    getUpDates('case', '59', false);
+})
+
